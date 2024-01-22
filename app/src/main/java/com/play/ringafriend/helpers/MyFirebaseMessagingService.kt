d9dd3c4414de9ba8a079ed2a1e2958a94d11730f
Joel <joel02varghese@gmail.com>
2024-01-22 16:20:45 +0530

    Open RingerActivity on notification

--- app/src/main/java/com/play/ringafriend/helpers/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/play/ringafriend/helpers/MyFirebaseMessagingService.kt
@@ -1,7 +1,10 @@
 package com.play.ringafriend.helpers
 
+import android.content.Intent
 import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import com.play.ringafriend.RingerActivity
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
     // [START on_new_token]
@@ -24,5 +27,36 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
     private fun sendRegistrationToServer(token: String?) {
         // TODO: Implement this method to send token to your app server.
         Log.d(TAG, "sendRegistrationTokenToServer($token)")
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        // TODO(developer): Handle FCM messages here.
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d(TAG, "From: ${remoteMessage.from}")
+
+        // Check if message contains a data payload.
+        if (remoteMessage.data.isNotEmpty()) {
+            Log.d(TAG, "Message data payload: ${remoteMessage.data}")
+            val intent = Intent(applicationContext, RingerActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+            startActivity(intent)
+//            // Check if data needs to be processed by long running job
+//            if (needsToBeScheduled()) {
+//                // For long-running tasks (10 seconds or more) use WorkManager.
+//                scheduleJob()
+//            } else {
+//                // Handle message within 10 seconds
+//                handleNow()
+//            }
+        }
+
+        // Check if message contains a notification payload.
+        remoteMessage.notification?.let {
+            Log.d(TAG, "Message Notification Body: ${it.body}")
+        }
+
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
     }
 }
