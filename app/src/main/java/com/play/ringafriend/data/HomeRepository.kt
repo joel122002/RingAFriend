822bf8f043b6dfe4d2a9e7d49e545e33221771f6
Joel <joel02varghese@gmail.com>
2024-02-08 20:37:11 +0530

    Implement interface functions

--- app/src/main/java/com/play/ringafriend/data/HomeRepository.kt
+++ app/src/main/java/com/play/ringafriend/data/HomeRepository.kt
@@ -33,4 +33,43 @@ class HomeRepository(context: Context) {
         })
         return data
     }
+
+    fun login(authModel: AuthModel): LiveData<Boolean> {
+        val data = MutableLiveData<Boolean>()
+
+        apiInterface?.login(authModel)?.enqueue(object : Callback<AuthModel>{
+            override fun onFailure(call: Call<AuthModel>, t: Throwable) {
+                data.value = false
+            }
+            override fun onResponse(call: Call<AuthModel>, response: Response<AuthModel>) {
+                if (response.code() == 204){
+                    data.value = true
+                }else{
+                    data.value = false
+                }
+            }
+        })
+        return data
+    }
+
+    fun profile(): LiveData<UserModel> {
+        var data = MutableLiveData<UserModel>()
+
+        apiInterface?.profile()?.enqueue(object : Callback<UserModel>{
+            override fun onFailure(call: Call<UserModel>, t: Throwable) {
+                data.value = null
+            }
+            override fun onResponse(call: Call<UserModel>, response: Response<UserModel>) {
+                val res = response.body()
+                if (response.code() == 200){
+                    data.value = res
+                }else if  (response.code() == 401) {
+                    data.value = UserModel(null, null, "Unauthorized")
+                } else {
+                    data.value = null
+                }
+            }
+        })
+        return data
+    }
 }
