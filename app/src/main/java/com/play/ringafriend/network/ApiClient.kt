b30950f1fd40562b8092f4f09d34ea3d37accd68
Joel <joel02varghese@gmail.com>
2024-02-08 12:22:34 +0530

    Create API Client for Retrofit

---
+++ app/src/main/java/com/play/ringafriend/network/ApiClient.kt
@@ -0,0 +1,32 @@
+package com.play.ringafriend.network
+
+import android.content.Context
+import com.play.ringafriend.BuildConfig
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+import java.util.concurrent.TimeUnit
+
+val BASEURL = BuildConfig.BACKEND_URL
+class ApiClient(val context: Context) {
+    private var retrofit: Retrofit?=null
+    fun getApiClient(): Retrofit {
+        val gson = GsonBuilder()
+            .setLenient()
+            .create()
+        val okHttpClient = OkHttpClient.Builder()
+            .readTimeout(100, TimeUnit.SECONDS)
+            .connectTimeout(100, TimeUnit.SECONDS)
+            .build()
+        if (retrofit == null) {
+            retrofit = Retrofit.Builder()
+                .baseUrl(BASEURL)
+                .client(okHttpClient)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build()
+        }
+        return retrofit!!
+    }
+}
