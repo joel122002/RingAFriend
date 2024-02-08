5003e69fd757a99f55ae4384513b1367e5ae278c
Joel <joel02varghese@gmail.com>
2024-02-08 20:38:48 +0530

    Add to login and profile to viewmodel

--- app/src/main/java/com/play/ringafriend/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/play/ringafriend/viewmodel/HomeViewModel.kt
@@ -4,13 +4,17 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.play.ringafriend.data.AuthModel
 import com.play.ringafriend.data.HomeRepository
 import com.play.ringafriend.data.RegisterDevicePostModel
+import com.play.ringafriend.data.UserModel
 
 class HomeViewModel(application: Application): AndroidViewModel(application){
 
     private var homeRepository: HomeRepository?=null
     var registerDeviceLiveData:LiveData<Boolean>?=null
+    var loginLiveData:LiveData<Boolean>?=null
+    var profileLiveData:LiveData<UserModel>?=null
 
     init {
         homeRepository = HomeRepository(application.applicationContext)
@@ -19,5 +23,13 @@ class HomeViewModel(application: Application): AndroidViewModel(application){
 
     fun registerDevice(registerDevicePostModel: RegisterDevicePostModel){
         registerDeviceLiveData = homeRepository?.registerDevice(registerDevicePostModel)
+    }
+
+    fun login(authModel: AuthModel){
+        loginLiveData = homeRepository?.login(authModel)
+    }
+
+    fun profile(){
+        profileLiveData = homeRepository?.profile()
     }
 }
