ebe83325f4392faa43cecabe1957e2710ba3bdb6
Joel <joel02varghese@gmail.com>
2024-03-01 22:38:41 +0530

    Fix volume not playing on OnePlus devices

--- app/src/main/java/com/play/ringafriend/RingerActivity.kt
+++ app/src/main/java/com/play/ringafriend/RingerActivity.kt
@@ -2,10 +2,13 @@ package com.play.ringafriend
 
 import android.app.KeyguardManager
 import android.content.Context
+import android.content.Intent
 import android.media.AudioManager
 import android.media.MediaPlayer
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
 import android.view.WindowManager
 import androidx.activity.ComponentActivity
@@ -17,18 +20,18 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalView
-import androidx.compose.ui.tooling.preview.Preview
 import com.play.ringafriend.ui.theme.RingAFriendTheme
 import kotlin.properties.Delegates
 
 val TAG = "FIREISCOOL"
+
 class RingerActivity : ComponentActivity() {
     var currentAudioVolume: Int by Delegates.notNull<Int>()
+    var currentAccessibilityVolume: Int by Delegates.notNull<Int>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // Open Activity even if screen locked
@@ -55,6 +58,20 @@ class RingerActivity : ComponentActivity() {
                 audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC),
                 0
             )
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                currentAccessibilityVolume =
+                    audioManager.getStreamVolume(AudioManager.STREAM_ACCESSIBILITY)
+                Log.d(
+                    TAG,
+                    "Vol is " + audioManager.getStreamVolume(AudioManager.STREAM_ACCESSIBILITY)
+                )
+                audioManager.setStreamVolume(
+                    AudioManager.STREAM_ACCESSIBILITY,
+                    audioManager.getStreamMaxVolume(AudioManager.STREAM_ACCESSIBILITY),
+                    0
+                )
+            }
+
             RingAFriendTheme {
                 KeepScreenOn()
                 // A surface container using the 'background' color from the theme
@@ -84,10 +101,17 @@ class RingerActivity : ComponentActivity() {
         super.onStop()
         val audioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
         Log.d(TAG, currentAudioVolume.toString())
-        if (currentAudioVolume >= 0){
+        if (currentAudioVolume >= 0) {
             audioManager.setStreamVolume(
                 AudioManager.STREAM_MUSIC,
                 currentAudioVolume,
+                0
+            )
+        }
+        if (currentAccessibilityVolume >= 0 && Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            audioManager.setStreamVolume(
+                AudioManager.STREAM_ACCESSIBILITY,
+                currentAccessibilityVolume,
                 0
             )
         }
