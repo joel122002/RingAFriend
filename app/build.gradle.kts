a9394136ddbc874c3f91cab8b63c2694372dbe5b
Joel <joel02varghese@gmail.com>
2024-07-10 01:42:32 +0530

    Create SocketClient singleton for socket io

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,6 +70,7 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
     implementation ("androidx.compose.runtime:runtime-livedata:1.6.1")
+    implementation ("io.socket:socket.io-client:2.0.0")
 
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
---
+++ app/src/main/java/com/play/ringafriend/network/SocketClient.kt
@@ -0,0 +1,33 @@
+package com.play.ringafriend.network
+
+import android.app.Activity
+import android.content.Context
+import android.widget.Toast
+import com.play.ringafriend.BuildConfig
+import io.socket.client.IO
+import io.socket.client.Manager
+import io.socket.client.Socket
+import io.socket.emitter.Emitter
+import okhttp3.OkHttpClient
+import java.util.concurrent.TimeUnit
+
+class SocketClient(val context: Context) {
+    companion object {
+        lateinit var socket: Socket
+        fun getClient(context: Context): Socket {
+            if (this::socket.isInitialized) {
+                return socket
+            }
+            val okHttpClient = OkHttpClient.Builder()
+                .addNetworkInterceptor(SetCookiesInterceptor(context))
+                .addNetworkInterceptor(ReceivedCookiesInterceptor(context))
+                .addNetworkInterceptor(AppVersionInterceptor())
+                .build()
+            val options = IO.Options()
+            options.webSocketFactory = okHttpClient
+            options.callFactory = okHttpClient
+            socket = IO.socket(BuildConfig.BACKEND_URL, options)
+            return socket
+        }
+    }
+}
